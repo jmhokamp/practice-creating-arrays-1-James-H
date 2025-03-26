849665186a2f61233ab7e50caded5fa99426cf01
jhokamp <jmhokamp@gmail.com>
2025-03-25 22:00:26 -0500

    updated

---
+++ arrays1.js
@@ -0,0 +1,15 @@
+
+//grocery list and price array
+let groceryList = ['beef', 'olive oil', 'frozen pizza', 'chicken', 'grapes']
+let itemPrices = [8.00, 12.00, 18.00]
+
+//calling array to display the entire array
+console.log(groceryList);
+console.log(itemPrices);
+
+console.log(groceryList[4]);
+console.log(groceryList[2]);
+console.log(groceryList[0]);
+
+console.log(itemPrices[0]);
+console.log(itemPrices[2]);
