afa482db5c2e1e61912b0c6f4310cb3c11128307
jhokamp <jmhokamp@gmail.com>
2025-03-25 22:02:19 -0500

    updated

--- arrays1.js
+++ arrays1.js
@@ -3,13 +3,15 @@
 let groceryList = ['beef', 'olive oil', 'frozen pizza', 'chicken', 'grapes']
 let itemPrices = [8.00, 12.00, 18.00]
 
-//calling array to display the entire array
+//printing array to display the entire array
 console.log(groceryList);
 console.log(itemPrices);
 
+//print 3 items from the array groceryList
 console.log(groceryList[4]);
 console.log(groceryList[2]);
 console.log(groceryList[0]);
 
+//print 2 items frome itemsPrice array
 console.log(itemPrices[0]);
 console.log(itemPrices[2]);
